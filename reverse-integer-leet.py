18f9c4c23accdfba85b72ab8bc8eaab9523f93cc
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-06-17 12:59:51 -0700

    added comments to reverse int problem

--- reverse-integer-leet.py
+++ reverse-integer-leet.py
@@ -3,18 +3,24 @@
 def reverse(x: int) -> int:
     x = str(x)
     string = ''
-    
+
+    # checks to see if number is negative
     if x[0] == '-':
             string += x[0]
-            
+    
+    # reverse string of ints
     for index in x[::-1]:
+        # skips the negatuve sign or zero at the end of string
         if index != '-' and x[len(x)-1] != 0:
             string += str(index)
-            
-    if int(string) >= 2147483648 or int(string) <= -2147483648:
+    
+    number = int(string)
+    
+    # checks to see if its within range
+    if number >= 2147483648 or number <= -2147483648:
         return 0 
         
-    return int(string)
+    return number
 
 if __name__ == '__main__':
     print(reverse(-101010101010101010101010101010))
